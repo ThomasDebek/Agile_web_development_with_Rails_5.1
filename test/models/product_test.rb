be23efe824add3f522abfa918a5a901422ac2d84
tom.debek@gmail.com <tom.debek@gmail.com>
2017-09-15 14:47:24 +0200

    Added testing to models products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  test 'Nie ma pustych pol' do
+  test 'Product attributes must not be empty' do
     product = Product.new
     assert product.invalid?
     assert product.errors[:title].any?
@@ -9,4 +9,28 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:price].any?
     assert product.errors[:image_url].any?
   end
+
+  test 'product price must be positive' do
+    product = Product.new(title: 'My book Title', description: 'yyy', image_url: 'zzz.jpg')
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+                 product.errors[:price]
+    product.price = 0
+    assert product.invalid?
+    assert_equal  ["must be greater than or equal to 0.01"],
+                  product.errors[:price]
+    product.price = 1
+    assert product.valid?
+
+  end
+
+
+
+
+
 end
+
+
+
