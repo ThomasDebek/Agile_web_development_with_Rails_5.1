3f1f27325fed03923302d109e0c0c87b046ef51f
tom.debek@gmail.com <tom.debek@gmail.com>
2017-11-21 21:48:18 +0100

    Nowa metoda, Potrzebujemy teraz inteligetnej metody, ktora spr czy nasza lista produktow zawiera juz dodany produkt, jesli tak, to potrąca ilość, a jeśli nie to buduje nowy element zamówienia

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,17 @@
 class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+
+  # Potrzebujemy teraz inteligetnej metody, ktora spr czy nasza lista produktow zawiera juz dodany produkt, jesli tak, to potrąca ilość, a jeśli nie to buduje nowy element zamówienia
+  def add_product(product)
+    current_item = line_items.find_by(product_id: product.id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product.id)
+    end
+    current_item
+  end
+
+
 end
